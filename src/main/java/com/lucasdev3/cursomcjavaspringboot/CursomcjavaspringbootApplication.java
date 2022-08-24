6e379dc12d1f2dfd0c05c21f08085b1436e6185e
lucasdev3 <lucasrepositoriodados@gmail.com>
2022-08-23 21:10:51 -0300

    start project sprint boot web

---
+++ src/main/java/com/lucasdev3/cursomcjavaspringboot/CursomcjavaspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lucasdev3.cursomcjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcjavaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcjavaspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lucasdev3/cursomcjavaspringboot/CursomcjavaspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasdev3.cursomcjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursomcjavaspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
